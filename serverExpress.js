f66376ff4557d6efbe2811d083fb70bae8e6d82d
Lis Insfran <tomlinumen@gmail.com>
2022-09-15 17:24:14 -0300

    Desafia 03: Servidores con Express

---
+++ serverExpress.js
@@ -0,0 +1,44 @@
+const Contenedor = require("./Contenedor");
+const express = require('express');
+const app = express();
+
+const PORT = 8080;
+const contenedor = new Contenedor("productos.json");
+
+app.get('/', (req, res) => {
+    res.send('Alo Express Server!')
+});
+
+app.get('/productos', async (req, res) => {
+    const allProducts = await contenedor.getAll();
+    res.json(allProducts);
+});
+
+app.get('/productoRandom', async (req, res) => {
+    const allProducts = await contenedor.getAll();
+    const maxId = allProducts.length;
+    
+    const randomNumber = generateRandomNumber(1, maxId);
+    const randomProduct = await contenedor.getById(randomNumber);
+
+    res.json(randomProduct);
+
+})
+
+app.get('/libros', (req, res) => {
+    res.send('Alo, estas en Libros')
+});
+
+app.get('/object', (req, res) => {
+    res.json({title: 'Monster', author: 'Naoki Urasawa'})
+});
+
+const generateRandomNumber = (min, max) => {
+    return Math.floor((Math.random() * (max+1 -min)) +min);
+}
+
+const server = app.listen(PORT, () => {
+    console.log(`>>>> Server started at http://localhost:${PORT}`)
+})
+
+server.on('error', (error) => console.log(error));
